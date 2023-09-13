bb1b23dbeb24774fe1676ef3558bb78ddc5c5c4c
adamgonda <adamgondagyula@gmail.com>
2023-09-13 10:54:25 +0200

    comply with strict setup

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+const host = process.env['HOST'] ?? 'localhost';
+const port = process.env['PORT'] ? Number(process.env['PORT']) : 3000;
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
   res.send({ message: 'Hello API' });
 });
 
