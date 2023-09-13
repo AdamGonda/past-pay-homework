cce070498902b9a960e583732e730abe654facef
adamgonda <adamgondagyula@gmail.com>
2023-09-13 14:37:35 +0200

    add e2e tests

--- e2e/src/server/server.spec.ts
+++ e2e/src/server/server.spec.ts
@@ -1,10 +1,26 @@
 import axios from 'axios';
 
-describe('GET /', () => {
-  it('should return a message', async () => {
-    const res = await axios.get(`/`);
+describe('Operations', () => {
 
-    expect(res.status).toBe(200);
-    expect(res.data).toEqual({ message: 'Hello API' });
-  });
+  const testCases = [
+    { endpoint: '/add', a: 5, b: 3, expected: '8' },
+    { endpoint: '/subtract', a: 5, b: 3, expected: '2' },
+    { endpoint: '/multiply', a: 5, b: 3, expected: '15' },
+    { endpoint: '/divide', a: 6, b: 3, expected: '2' },
+  ];
+
+  test.each(testCases)(
+    'should correctly compute for %s',
+    async ({ endpoint, a, b, expected }) => {
+      const res = await axios.get(`http://localhost:3000${endpoint}`, {
+        params: {
+          a: a.toString(),
+          b: b.toString(),
+        },
+      });
+
+      expect(res.status).toBe(200);
+      expect(res.data.result).toEqual(expected);
+    }
+  );
 });
